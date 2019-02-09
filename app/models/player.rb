ecb5cb80e413d00f7a2213ca777d02864677d1ed
jriv95 <joel.rivera7995@gmail.com>
2019-02-09 18:14:36 -0500

    Added serializers and cors

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ApplicationRecord
     belongs_to :position
+
+    def position_name
+        self.position.name
+    end
 end
---
+++ app/serializers/player_serializer.rb
@@ -0,0 +1,3 @@
+class PlayerSerializer < ActiveModel::Serializer
+  attributes :id, :name, :games_played, :game_minutes, :points, :FG_percentage, :three_point_percentage, :FT_percentage, :position_name
+end
